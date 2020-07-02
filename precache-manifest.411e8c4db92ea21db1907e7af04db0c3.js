328118fc8fbcf2049c412abb65ebcd4052d9a6c9
semih <semihensar61@gmail.com>
2020-07-03 00:46:07 +0300

    Updates

---
+++ precache-manifest.411e8c4db92ea21db1907e7af04db0c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4204a9c2c0ee5be9c0ae6a6171e2e3a2",
+    "url": "/my-holonext-task/index.html"
+  },
+  {
+    "revision": "d886b83ab93bb8527969",
+    "url": "/my-holonext-task/static/css/main.b6ac47c1.chunk.css"
+  },
+  {
+    "revision": "2bbf5fe05ca9982947da",
+    "url": "/my-holonext-task/static/js/2.4666c410.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/my-holonext-task/static/js/2.4666c410.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d886b83ab93bb8527969",
+    "url": "/my-holonext-task/static/js/main.0fb6c4f6.chunk.js"
+  },
+  {
+    "revision": "44e7fc32b1115e4603da",
+    "url": "/my-holonext-task/static/js/runtime-main.f187ac22.js"
+  }
+]);
